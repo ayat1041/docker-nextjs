e42de6bd63c3c363b6615878b583b6d12ea0721a
ayat1041 <ayat.dihan6593@gmail.com>
2024-10-29 23:21:33 +0600

    demo calculator color update

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="bg-slate-900 p-10 flex flex-col items-center gap-4 rounded-xl shadow-[#ffffff10] shadow-lg">
-        <div className="p-8 bg-slate-700 text-3xl font-bold w-56 text-center rounded-sm">
+        <div className="p-8 bg-slate-200 text-3xl font-bold w-56 text-center rounded-sm">
           {count}
         </div>
         <div className="w-full flex gap-4">
