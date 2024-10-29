a222a0a758dd3d5004e1d0fe674e06861707b0d0
ayat1041 <ayat.dihan6593@gmail.com>
2024-10-29 22:21:59 +0600

    complete docker setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
+// next.config.ts
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+    domains: [], // if external images are used, add domains here
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
 };
 
 export default nextConfig;
